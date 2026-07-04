67f419b5acb6840f46710d5bc6738e932ae5dd7a
guru168-max <dajigurulute@gmail.com>
2026-07-04 16:22:14 +0530

    Implement binary search for next greatest letter

---
+++ Binary Search/find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,22 @@
+  class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int start=0;
+        int end=letters.size()-1;
+        int ans=0;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(letters[mid]>target) 
+            {
+                ans=mid;
+                end=mid-1;
+            }
+            else
+            {
+                start=mid+1;
+            }
+        }
+        return letters[ans];
+    }
+};
