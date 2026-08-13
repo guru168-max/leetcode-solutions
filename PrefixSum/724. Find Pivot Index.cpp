6b4be3b32abf060f0944de01d9657618e61559c9
guru168-max <dajigurulute@gmail.com>
2026-08-13 11:17:19 +0530

    Implement pivot index finding algorithm

---
+++ PrefixSum/724. Find Pivot Index.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n=nums.size();
+        int total=accumulate(nums.begin(), nums.end(), 0);
+        //total=LEFT+PIVOT+RIGHT
+        //LEFT=TOTAL-LEFT-PIVOT(current element)
+        int leftSum=0;
+        for(int i=0; i<n; i++)
+        {
+            if(leftSum==total-leftSum-nums[i])
+            {
+                return i;
+            }
+            leftSum+=nums[i];
+
+        }
+        return -1;
+        
+    }
+};
