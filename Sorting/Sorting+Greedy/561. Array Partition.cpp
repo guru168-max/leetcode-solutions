f3835a83180ae279703e7aa67394b318b3264bef
guru168-max <dajigurulute@gmail.com>
2026-09-17 11:06:05 +0530

    Add solution for Array Partition problem
    Implement the arrayPairSum function to maximize the sum of min pairs.

---
+++ Sorting/Sorting+Greedy/561. Array Partition.cpp
@@ -0,0 +1,49 @@
+// Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn)
+//such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
+// Example 1:
+
+// Input: nums = [1,4,3,2]
+// Output: 4
+// Explanation: All possible pairings (ignoring the ordering of elements) are:
+// 1. (1, 4), (2, 3) -> min(1, 4) + min(2, 3) = 1 + 2 = 3
+// 2. (1, 3), (2, 4) -> min(1, 3) + min(2, 4) = 1 + 2 = 3
+// 3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
+// So the maximum possible sum is 4.
+
+// Example 2:
+
+// Input: nums = [6,2,6,5,1,2]
+// Output: 9
+// Explanation: The optimal pairing is (2, 1), (2, 5), (6, 6). min(2, 1) + min(2, 5) + min(6, 6) = 1 + 2 + 6 = 9.
+
+// Constraints:
+
+//     1 <= n <= 104
+//     nums.length == 2 * n
+//     -104 <= nums[i] <= 104
+
+ 
+
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        //1,2,3,4
+        //1,2,2,5,6,6
+        // each pair of we need to take min 
+        //like min(a1,b1)+min(a2,b2)+....
+        //if we are going to take min from each of 
+        //so if we sort and then take from each of group the first element of the pair 
+        //this will sound like i+=2 take 
+        //it mean thaat the first one only contribution for the sum
+
+        int n=nums.size();
+        sort(nums.begin(), nums.end());
+        int sum=0;
+        for(int i=0; i<n; i+=2)
+        {
+            sum+=nums[i];
+        }
+        return sum;
+        
+    }
+};
