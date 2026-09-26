f8d0a367985398548ef9a80a9e2eea94baf0ee3f
guru168-max <dajigurulute@gmail.com>
2026-09-26 22:36:44 +0530

    Add solution for Valid Palindrome II
    Implement a solution to check if a string can be a palindrome after deleting at most one character. This includes a helper function to validate palindromes.

---
+++ Two Pointe/680. Valid Palindrome II.cpp
@@ -0,0 +1,67 @@
+// Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+ 
+
+// Example 1:
+
+// Input: s = "aba"
+// Output: true
+
+// Example 2:
+
+// Input: s = "abca"
+// Output: true
+// Explanation: You could delete the character 'c'.
+
+// Example 3:
+
+// Input: s = "abc"
+// Output: false
+
+ 
+
+// Constraints:
+
+//     1 <= s.length <= 105
+//     s consists of lowercase English letters.
+
+
+class Solution {
+public:
+    bool checkPalindrom(string &s, int start, int end)
+    {
+        while(start<=end)
+        {
+            if(s[start]!=s[end])
+            return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        //
+        //at most one char do we need to delete/skip from it
+        //i just confirm that if this current char are trying to oppose for making 
+        //palinddrom then need to skip and check rest of same for the check on both sides
+        //but when the from the left side we need to pass current+1 and same the the right prt
+        //for the right one right+1 and start as it
+
+        int n=s.size();
+        int start=0;
+        int end=n-1;
+        while(start<end)
+        {
+            if(s[start]==s[end])
+            {
+                start++;
+                end--;
+            }
+            else if(s[start]!=s[end])
+            {
+                return checkPalindrom(s,start+1,end)||checkPalindrom(s,start,end-1);
+            }
+        }
+        return true;
+    }
+};
